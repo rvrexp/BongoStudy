59887d9d0e3209c727acf6943bda7bbf6f462bd6
Richu Raju <richurajuv@outlook.com>
2024-11-04 16:12:35 -0600

    Add project files.

---
+++ Bongo.Models.Tests/DateInFutureAttributeTests.cs
@@ -0,0 +1,31 @@
+﻿using Bongo.Models.ModelValidations;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.Models.Tests
+{
+    [TestFixture]
+    public class DateInFutureAttributeTests
+    {
+        [TestCase(100, ExpectedResult = true)]
+        [TestCase(-100, ExpectedResult = false)]
+        [TestCase(0, ExpectedResult = false)]
+        public bool DateValidator_InputExpectedDateRange_DateValidity(int addTime)
+        {
+            DateInFutureAttribute dateInFutureAttribute = new(() => DateTime.Now);
+
+            return dateInFutureAttribute.IsValid(DateTime.Now.AddSeconds(addTime));
+        }
+        [Test]
+        public void DateValidator_NotValidDate_ReturnErrorMessage()
+        {
+            var result = new DateInFutureAttribute();
+            ClassicAssert.AreEqual("Date must be in the future", result.ErrorMessage);
+        }
+    }
+}
---
+++ Bonko.DataAccess/Repository/IRepository/IStudyRoomBookingRepository.cs
@@ -0,0 +1,11 @@
+﻿using Bongo.Models.Model;
+
+namespace Bongo.DataAccess.Repository.IRepository
+{
+    public interface IStudyRoomBookingRepository
+    {
+        void Book(StudyRoomBooking booking);
+        IEnumerable<StudyRoomBooking> GetAll(DateTime? dateTime);
+        
+    }
+}
